4a611a32d62e3797dbe36ff2834459061c811eb1
devhenode <devodehenry.2000@gmail.com>
2023-12-12 15:56:23 +0100

    first

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <h1>{{ title }}</h1><br>
+  <p>Welcome...</p>
+
+  <div v-if="showModal">
+    <Modal @close="toggleModalTwo">
+
+      <template v-slot:links>
+        <a href="#">First link</a>
+        <a href="#">Second link</a>
+      </template>
+      <h1>This is under the Slot template</h1>
+    </Modal>
+
+  </div><br>
+  <button @click="toggleModalTwo">Show Modal</button>
+</template>
+
+
+<script>
+import Modal from './components/Modal.vue'
+
+  export default {
+    name: 'App',
+    components: { Modal },
+    data() {
+      return {
+        title: 'My First Vue Code :)'
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal">
+            <p>Modal content</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        closeModal() {
+            this.$emit('close')
+        }
+    }
+}
+</script>
+
+<style>
+    .backdrop {
+        top: 0;
+        position: fixed;
+        background: rgba(0, 0, 0, 0.5);
+        width: 100%;
+        height: 100%;
+    }
+    .modal {
+        width: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        background: white;
+        border-radius: 10px;
+    }
+    
+</style>
